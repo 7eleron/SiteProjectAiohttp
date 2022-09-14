b24522958c93856d6a9462f6fd2d1c653882a6ae
7eleron <7eleron@gmail.com>
2022-09-14 19:57:21 +0300

    add connection database

--- demo/app.py
+++ demo/app.py
@@ -2,6 +2,7 @@ import jinja2
 from aiohttp import web
 from .routes import setup_routes
 import aiohttp_jinja2
+import asyncpgsa
 
 
 async def create_app(config: dict):
@@ -12,4 +13,15 @@ async def create_app(config: dict):
         loader=jinja2.PackageLoader('demo', 'templates')
     )
     setup_routes(app)
+    app.on_startup.append(on_start)
+    app.on_cleanup.append(on_shutdown)
     return app
+
+
+async def on_start(app):
+    config = app['config']
+    app['db'] = await asyncpgsa.create_pool(dsn=config['database_url'])
+
+
+async def on_shutdown(app):
+    await app['db'].close()
